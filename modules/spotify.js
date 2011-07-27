6133351555787b1833c538a6759ee9653a89c359
Andrée Hansson <peolanha@gmail.com>
2011-07-27 18:26:32 +0200

    Added `.spotifind <track|album|artist>:<query>` search, added error handling.

--- modules/spotify.js
+++ modules/spotify.js
@@ -1,5 +1,6 @@
 var targetHost = "ws.spotify.com",
-    targetPath = "/lookup/1/.json?uri=spotify:",
+    targetLookupPath = "/lookup/1/.json?uri=spotify:",
+    targetSearchPath = "/search/1/",
     availTargets = ['SE', 'US'/*, 'AUS', 'CA'*/],
     http = require('http');
 
@@ -57,10 +58,26 @@ function createBitlyURL(api, type, id, cb) {
     });
 }
 
-function request(spotifyURI, cb) {
+function search(type, str, cb) {
+    var path = targetSearchPath + type + '.json?q=' + str;
+
+    request(path, cb);
+}
+
+function lookUp(type, id, cb) {
+    var path = targetLookupPath + type + ':' + id;
+
+    if (type === 'artist') {
+        path += '&extras=album';
+    }
+
+    request(path, cb);
+}
+
+function request(path, cb) {
     var chunks = "",
         server = http.createClient(80, targetHost),
-        request = server.request('GET', targetPath + spotifyURI, { 'host': targetHost });
+        request = server.request('GET', path, { 'host': targetHost });
 
     request.end();
 
@@ -78,19 +95,22 @@ function request(spotifyURI, cb) {
 }
 
 function respond(api, message) {
-    var type = message.match_data[1],
-        id = message.match_data[2],
-        uri = type + ':' + id;
+    var type = message.match_data[ 1 ],
+        id = message.match_data[ 2 ];
 
-    if (type === 'artist') {
-        uri += '&extras=album';
-    }
-
-    request(uri, function(data) {
-        var format = formatting[ data.info.type ]( data );
+    lookUp(type, id, function(data) {
+        var msg = '';
+        
+        if ( data.info ) {
+            msg = ' ' + formatting[ data.info.type ]( data );
+        }
+        else {
+            msg = 'Invalid Spotify URL (nothing found)';
+            id = '';
+        }
 
         createBitlyURL(api, type, id, function(bitlyURL) {
-            api.jerk.say(message.source, bitlyURL + ' ' + format);
+            api.jerk.say(message.source, bitlyURL + msg);
         });
     });
 };
@@ -102,4 +122,20 @@ exports.invoke = function(api) {
 
     api.jerk.watch_for(/open\.spotify\.com\/(track|artist|album)\/([a-zA-Z0-9]+)/i, wrap);
     api.jerk.watch_for(/spotify:(track|artist|album):([a-zA-Z0-9]+)/i, wrap);
+    api.jerk.watch_for(/^\.spotifind (track|artist|album):(.+)$/i, function(message) {
+        var type = message.match_data[ 1 ],
+            searchStr = encodeURI( message.match_data[ 2 ] );
+
+        search(type, searchStr, function(data) {
+            var d = data[ type + 's' ][ 0 ];
+
+            if ( d ) {
+                message.match_data[ 2 ] = d.href.split(':')[ 2 ];
+                wrap( message );
+            }
+            else {
+                api.jerk.say(message.source, 'Nothing matching that query found on Spotify :-(');
+            }
+        });
+    });
 }
