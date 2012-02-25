75a96d9db405035127bfab14a0a23c5c5736472e
Andrée Hansson <peolanha@gmail.com>
2012-02-25 23:36:54 +0100

    Added example config file, updated readme

--- README.md
+++ README.md
@@ -8,16 +8,16 @@ URIs and lets everyone know what artist, track or album that was linked.
 
 How to get started
 ------------------
-This project expects 0.4.0 of node.js or newer, but it'll probably work
+This project expects version 0.6.0+ of node.js, but it'll probably work
 work with lower versions if you fix the `node_modules` reference.
 
 1. Go into your Terminal application and do
    `git clone git@github.com:peol/socialhapy.git`
 2. `cd socialhapy` and then initialize the required submodules needed by
    running `git submodule init` and `git submodule update --recursive`
-3. Edit the `socialhapy/config.js` file to your liking
+3. Edit the `socialhapy/config.example.js` file to your liking, and **save it as `config.js`**
 4. Run socialhapy by either running it with `node socialhapy.js` or by
-   the recommended way, with `node-forever` or `nohup sh socialhapy.sh &`
+   the recommended way, with `forever`
 
 Create your own socialhapy module
 ---------------------------------
@@ -25,8 +25,6 @@ socialhapy has its own module system that leverages the `require` system but
 demands that an API is returned with at least a `register` method in it, it's
 also required that you add the module in `config.js`, see other entries under
 the `modules` object.
-
-For a more in=depth guidance, see `examples/simple-module.js`.
 
 Roadmap
 -------
---
+++ config.example.js
@@ -0,0 +1,88 @@
+module.exports = {
+    // Currently, this flag only restricts which channels the
+    // bot will join (e.g. `true` will ignore all entries but `debug`
+    // in the below channel object)
+    debugMode: false,
+
+    channels: {
+        idle: [
+            '#list',
+            '#of',
+            '#channels',
+            '#to',
+            '#idle',
+            '#in'
+        ],
+        debug: ['#socialhapy-dev']
+    },
+
+    irc: {
+        server: 'chat.freenode.net',
+        nick: 'socialhapy' + ~~(Math.random() * 1001),
+        log: false,
+        user: {
+            username: 'socialhapy',
+            realname: 'Social bot for developers'
+        },
+
+        // Automatically populated by combining all arrays in `channels`
+        // WILL BE OVERWRITTEN
+        channels: []
+    },
+
+    // The prefix used on ALL messages that goes through functions.format(true, '....')
+    prefix: '★ ',
+
+    modules: {
+        core: {
+            enabled: true
+        },
+
+        bitly: {
+            enabled: false,
+            user: 'my-bitly-user',
+            token: 'my-bitly-token'
+        },
+
+        twitter: {
+            enabled: false,
+            // These are only needed for the stream functionality
+            consumer_key: '',
+            consumer_secret: '',
+            access_token_key: '',
+            access_token_secret: '',
+            // Array of people you want to follow, this should contain ALL users
+            // You can filter out specific users by channels below
+            // Note: Lowercase on all usernames!
+            users: [
+                'peolanha'
+            ],
+            // Define stream output. THE CHANNELS NEEDS TO BE ADDED IN `channels` above
+            // Syntax: "#channel": [] (will output everything) or ['twitterer1', 'twitterer2']
+            // The bot will not output streaming tweets to the normal channel list, if you want
+            // it to stream to your channel, you NEED to add it to this object 
+            // TODO: Add these channels dynamically in `register` in `modules/twitter.js`
+            streamChannels: {
+            }
+        },
+
+        github: {
+            enabled: true
+        },
+
+        spotify: {
+            enabled: true,
+            compatible: []
+        }
+    },
+
+    admins: {
+        // nick (required)
+        'my-nickname': {
+            // ident (optional)
+            user: '~ident',
+            // host (optional)
+            host: 'hostname.com'
+        }
+    }
+};
